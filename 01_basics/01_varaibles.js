5940911452f99cad63bfa7adda3422e417c96a57
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-08-27 18:54:56 +0000

    variable

---
+++ 01_basics/01_varaibles.js
@@ -0,0 +1,11 @@
+const accountId=144553
+let accountEmail="jhamuari.gmail.com"
+var accountpassword="1234"
+ accountCity="darbhanga"
+
+ //accountId=2
+ accountEmail="jhasubhash@gmail.com"
+accountpassword="5678"
+  accountCity="patna"
+ console.table([accountId,accountEmail,accountpassword,accountCity])
+ 
