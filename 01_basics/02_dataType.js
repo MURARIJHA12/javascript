c02733ee44d3d346d66b2b6e93c20e1333dee034
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-08-27 19:29:02 +0000

    container daconfig done

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -2,4 +2,25 @@
 
 //alert(3+2) we are not using nodejs,not broswer
 
-console.log(3+4); console.log("Murari");
+console.log(3+4); 
+console.log("Murari");
+
+let name="Murari"
+let age=23
+let isLonggedIn=false
+
+
+//number=>2 to 53;
+//bigint
+//string =>""
+//bollean =>true/false
+//null =>satnder value
+//undefined->value are not assigned
+//Symbol ->unique
+
+//object
+
+console.log(typeof "Murari");
+ console.log(typeof null)
+console.log(typeof undefined)
+ console.log(typeof 12);
