3430717c4880dbca06d45b863d91590269d4c375
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-09-01 19:53:45 +0000

    math and num

---
+++ 01_basics/06nums_and_math.js
@@ -0,0 +1,42 @@
+/*const score=400;
+console.log(score);
+
+const balance=new Number(100);
+console.log(balance);
+
+console.log(balance.toFixed(2));//result=100.00;
+
+const othernumber=123.8966;
+
+console.log(othernumber.toPrecision(3));  // round above number=124;
+
+const hundereds=1000000;
+console.log(hundereds.toLocaleString('en-IN')); // output=10,00,000;
+
+*/
+
+//++++++++++++++++++++++++++++++++ Math+++++++++++++++++++;
+
+
+console.log(Math);
+console.log(Math.abs(-4));//conversion of negative to possitive output=4
+
+console.log(Math.round(4.6));//round above number output=5;
+
+console.log(Math.ceil(4.1));//top of number output=5;
+
+console.log(Math.floor(4.9)); //least number in the integer output=4;
+
+console.log(Math.sqrt(25));//find square root of any no output=5;
+
+console.log(Math.min(4,6,5,2,78,56,7));//find the maximum value of an array output=2;
+
+console.log(Math.max(4,6,5,2,78,56,7));//find the maximum value of an array output=78;
+
+console.log(Math.random());//value are between 0 to 1;
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
