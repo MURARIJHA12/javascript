d9abef542e9748be8693c4621b141af90ffa2a36
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-09-02 19:06:44 +0000

    array01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,29 @@
+//array
+
+const myarr=[0,1,2,3,4,5];
+
+console.log(myarr[1]);
+
+//method of an array
+
+//push
+myarr.push(6);//array element are added
+myarr.push(7);
+console.log(myarr);
+
+//pop
+myarr.pop();//delete the least element
+console.log(myarr);
+
+
+//unshift
+myarr.unshift(9);//first element are shited 0->9;
+console.log(myarr);//output=9,0,1,2,3,4,5,6
+
+//shift
+myarr.shift();
+console.log(myarr);//output=0,1,2,3,4,5,6;
+
+
+
+
