1f522a0d259d956a6581e5b83cb6d28ec050f6c6
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-09-03 07:14:25 +0000

    Array01

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -24,6 +24,43 @@ console.log(myarr);//output=9,0,1,2,3,4,5,6
 myarr.shift();
 console.log(myarr);//output=0,1,2,3,4,5,6;
 
+const newarr=myarr.join();
+console.log(newarr);
 
 
+
+// //slice,splice
+
+//slice
+console.log("A",myarr);
+
+const myn1=myarr.slice(1,3)
+
+console.log(myn1);
+
+console.log("B",myarr);
+
+
+//splice
+
+const myn2=myarr.splice(1,3)
+console.log("c",myarr);
+console.log(myn2);
+
+
+//output=
+/*
+A [
+  0, 1, 2, 3,
+  4, 5, 6
+]
+[ 1, 2 ]
+B [
+  0, 1, 2, 3,
+  4, 5, 6
+]
+c [ 0, 4, 5, 6 ]
+[ 1, 2, 3 ]
+
+*/
 
