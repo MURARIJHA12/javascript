2d1e4a34f7814cb9cec8d64533251068444f00c3
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-10 02:29:43 +0000

    IIFE

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,10 @@
+//Immediately Invoked Function Expresion(IIFE)
+/*
+function Murari(){
+    console.log(`DB CONNECTED`);
+}
+Murari();
+*/
+(function Murari(){
+    console.log(`DB CONNECTED`);
+})();
