ba0127119623fa92cb202d08b963149cae47ae40
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-10 06:37:50 +0000

    While and Do While

---
+++ 05_iterations/two.js
@@ -0,0 +1,37 @@
+//While loops
+/*
+let i=0;
+while(i<=10){
+
+    console.log(`value of index is ${i}`);
+    i=i+2;
+}
+
+*/
+
+/*
+
+let myArray=['Murari','subhash','suraj','rakesh'];
+let i=0;
+while(i<myArray.length){
+    console.log(`valueis my arrays${i+" "+myArray[i]}`);
+    i++;
+}
+
+*/
+
+//do while loops 
+
+//syntax:
+/*
+do{
+
+}while(condition);
+*/
+
+let score=11;
+do{
+    console.log(`Score is ${score}`)
+    score++;
+}
+while(score<=10);
