237af9505833b498940f8cc9b3dd5c27210b49f4
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-10 12:19:19 +0000

    For In loops

---
+++ 05_iterations/fourth.js
@@ -0,0 +1,28 @@
+/*
+const myObject={
+    js:'javaScript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:"Swift by apple"
+}
+
+for (const key in myObject) {
+
+    console.log(`${key}:${myObject[key]}`);
+    
+}
+*/
+
+//it is not possible
+/*
+const map=new Map();
+map.set('In',"India");
+map.set('USA',"United States of America");
+map.set('Fr',"France");
+map.set('IN',"India");
+
+for (const key in map) {
+    console.log(key);
+}
+    */
+
