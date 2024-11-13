29f17203ee4d0fbbbcabf78359cc229de3fb539d
MURARIJHA12 <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-13 18:10:20 +0000

    Filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,27 @@
+// const coding=["JS","ruby","Java","Python","Cpp"];
+
+// const value=coding.foreach((item)=>{
+//     console.log(item);
+//     return item;
+    
+// });
+// console.log(value);
+
+
+const myNums=[1,2,3,4,5,6,7,8,9,10];
+
+// const newNums=myNums.filter((num)=>num>4)
+//console.log(newNums); output:[ 5, 6, 7, 8, 9, 10 ]
+/*
+const newNums=myNums.filter((num)=>{
+    num>4
+})
+console.log(newNums);//Output:[]
+*/
+
+
+const newNums=myNums.filter((num)=>{
+  return  num>4
+})
+console.log(newNums);//Output:[ 5, 6, 7, 8, 9, 10 ]
+
