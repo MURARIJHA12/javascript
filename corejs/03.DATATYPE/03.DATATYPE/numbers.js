35632a79400af281cdcec7a2e8ace0485acfda5a
Murari Kumar Jha <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-27 20:26:33 +0530

    Add files via upload

---
+++ corejs/03.DATATYPE/03.DATATYPE/numbers.js
@@ -0,0 +1,22 @@
+//module 1
+
+let num=101;
+let float=10.25;
+
+console.log(typeof num);
+console.log(typeof float);
+
+
+
+//model 2
+
+//+,-,*,/
+
+let num1=10;
+let num2=20;
+
+console.log(num1+num2);
+
+//module 3
+
+console.log(num+mum2*100/2-20);
