819b59c43796eb5da564fc143d386c5be715d240
Murari Kumar Jha <147914105+MURARIJHA12@users.noreply.github.com>
2024-11-28 23:47:59 +0530

    Add files via upload

---
+++ corejs/03.DATATYPE/03.DATATYPE/String.js
@@ -0,0 +1,31 @@
+//string
+
+let msg1="Hello";
+let msg2="Murari";
+console.log(msg1,msg2);
+
+//document.getElementById('root').innerHTML=`<h1>${msg1} ${msg2}</h1>`//Hello Murari
+
+/*
+document.getElementById('root').innerHTML=`<h1>${msg1} ${msg2}</h1>`
+document.getElementById('root').innerHTML=`<h1>kaisa ho aap</h1>`
+//kaisa ho aap
+*/
+
+//image 
+
+//document.getElementById('root').innerHTML=`<img src="Subhash.jpg" width="250px" height="250px" alt="js image">`;
+
+
+//email
+/*
+document.getElementById('root').innerHTML = `<input type="email" name="email" placeholder="Enter email id">`;
+
+document.Write("<h1 id='data'>nwe data</h1>")
+let data=document.getElementById('data').innerHTML;
+console.log(typeof (data));
+
+*/
+//function 
+console.log(msg1.toUpperCase());
+console.log(msg1.toLowerCase());
