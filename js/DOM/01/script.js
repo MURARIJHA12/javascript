bcc7e7b4cb646a4b731e8836d875c93a3051890e
Murari Kumar Jha <147914105+MURARIJHA12@users.noreply.github.com>
2024-12-11 21:33:41 +0530

    Add files via upload

---
+++ js/DOM/01/script.js
@@ -0,0 +1,14 @@
+// let smallImage=document.getElementsByClassName("oldImg");
+
+// for(let i=0;i<smallImage.length;i++){
+//     smallImage[i].src="SpiredMain.jpg";
+//    console.log(`value of image no.${i} is changed `);
+// }
+
+//selector
+
+ console.log(document .querySelector('h1'))//first paragraph selectors
+ console.log(document.querySelector('#description'));//select first element with id=description
+ console.log(document.querySelector('.box'));//select first element with class=box;
+ console.log(document.querySelectorAll("p"));//select all  element with pragraph;
+
